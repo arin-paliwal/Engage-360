0b8b267d78dbe80a11f04bae39415233db51c674
Arin Paliwal <paliwalarin09@gmail.com>
2024-12-05 09:36:00 +0530

    fix: Types Error Production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class",
-  content: [
-    './src/**/*.{html,js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        lightMode: {
-          background: "#fafafa",
-          secondaryBackground: "#f0f0f0",
-          primaryText: "#18181b",
-          secondaryText: "#71717a",
-          accentBlue: "#3C41E9",
-          accentGreen: "#12A779",
-          accentOrange: "#FFA40A",
-          accentPurple: "#913AF6",
-          accentLightBlue: "#427DDC",
-        },
-        darkMode: {
-          background: "#09090b",
-          secondaryBackground: "#161617",
-          primaryText: "#ffffff",
-          secondaryText: "#a1a1aa",
-          accentBlue: "#427ddc",
-          accentGreen: "#0C7B5A",
-          accentOrange: "#CC8308",
-          accentPurple: "#732BBF",
-          accentLightBlue: "#315BB0",
-        },
-        borders: {
-          primary: "#e5e7eb",
-          secondary: "#525252",
-        },
+export const darkMode = "class";
+export const content = [
+  './src/**/*.{html,js,ts,jsx,tsx}',
+];
+export const theme = {
+  extend: {
+    colors: {
+      lightMode: {
+        background: "#fafafa",
+        secondaryBackground: "#f0f0f0",
+        primaryText: "#18181b",
+        secondaryText: "#71717a",
+        accentBlue: "#3C41E9",
+        accentGreen: "#12A779",
+        accentOrange: "#FFA40A",
+        accentPurple: "#913AF6",
+        accentLightBlue: "#427DDC",
+      },
+      darkMode: {
+        background: "#09090b",
+        secondaryBackground: "#161617",
+        primaryText: "#ffffff",
+        secondaryText: "#a1a1aa",
+        accentBlue: "#427ddc",
+        accentGreen: "#0C7B5A",
+        accentOrange: "#CC8308",
+        accentPurple: "#732BBF",
+        accentLightBlue: "#315BB0",
+      },
+      borders: {
+        primary: "#e5e7eb",
+        secondary: "#525252",
       },
     },
   },
-  plugins: [],
 };
+export const plugins = [];
