1467539d5f7bdc05fe36d28a31402d08bb682577
Arin Paliwal <paliwalarin09@gmail.com>
2024-12-07 05:33:50 +0530

    Added To-Do Empty

--- src/components/employee/todo/to-do-list.tsx
+++ src/components/employee/todo/to-do-list.tsx
@@ -1,39 +1,43 @@
-import { useDrop } from 'react-dnd'
-import { Todo } from '../main/to-do'
-import { TodoItem } from './to-do-item'
+import { useDrop } from "react-dnd";
+import { Todo } from "../main/to-do";
+import { TodoItem } from "./to-do-item";
 
 interface TodoListProps {
-  title: string
-  todos: Todo[]
-  onComplete: (id: string) => void
-  onDrop?: (todo: Todo) => void
+  title: string;
+  todos: Todo[];
+  onComplete: (id: string) => void;
+  onDrop?: (todo: Todo) => void;
 }
 
 export function TodoList({ title, todos, onComplete, onDrop }: TodoListProps) {
   const [{ isOver }, drop] = useDrop(() => ({
-    accept: 'TODO',
+    accept: "TODO",
     drop: (item: Todo) => {
-      onDrop?.(item)
+      onDrop?.(item);
     },
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
-  }))
+  }));
 
   return (
     <div
       ref={drop}
-      className={`flex-1 h-[60vh] componentScroll overflow-auto rounded-lg border border-dashed border-zinc-200 p-4 transition-colors ${
-        isOver ? 'border-primary bg-primary/5' : 'bg-zinc-50/50'
+      className={`flex-1 h-[65vh] bg-lightMode-background dark:bg-darkMode-background componentScroll overflow-auto rounded-lg border-2 border-borders-primary dark:border-borders-secondary p-4 transition-colors ${
+        isOver ? "border-dotted" : "border-transparent"
       }`}
     >
-      <h2 className="mb-4 text-lg font-semibold text-zinc-900">{title}</h2>
-      <div className="space-y-3">
+      <h2 className="mb-4 text-lg font-semibold">{title}</h2>
+      <div className="space-y-3 h-full">
+        {todos.length === 0 && (
+          <div className="flex justify-center items-center h-full">
+            <img src="/images/empty.svg" alt="empty" className="" width={350} />
+          </div>
+        )}
         {todos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} onComplete={onComplete} />
         ))}
       </div>
     </div>
-  )
+  );
 }
-
