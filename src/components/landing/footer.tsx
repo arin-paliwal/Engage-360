c775a71714b74de0d54f90d1b5a728b48b9e6ac5
Arin Paliwal <paliwalarin09@gmail.com>
2024-12-11 05:17:36 +0530

    Fixed Footer

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
           {/* Map */}
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30773484.55170563!2d61.0245165611659!3d19.69009515037612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1733263459274!5m2!1sen!2sin"
-            width="100%"
+            // width="100%"
             height="300"
             className="border-0 rounded-md"
             loading="lazy"
@@ -48,7 +48,7 @@ const Footer = () => {
         </div>
 
         {/* Links Section */}
-        <div className="flex flex-col sm:flex-row gap-6 sm:gap-[4rem]">
+        <div className="flex flex-col lg:flex-row gap-6 sm:gap-[4rem]">
           <div className="flex flex-col gap-2 w-full sm:w-[15rem]">
             <h1 className="text-xl font-semibold">Quick Links</h1>
             <hr className="w-full h-[0.1rem] my-4 bg-secondary border-0 rounded" />
